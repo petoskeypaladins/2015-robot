f1ef5b3ccc8745243a221241e1e6d8587239be37
shannonc52 <shannoncartwright52@gmail.com>
2015-02-16 17:53:11 -0500

    Started OI

--- src/org/usfirst/frc/team3618/robot/OI.java
+++ src/org/usfirst/frc/team3618/robot/OI.java
@@ -1,13 +1,24 @@
 package org.usfirst.frc.team3618.robot;
 
+
+
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+
 import org.usfirst.frc.team3618.robot.commands.ExampleCommand;
+import org.usfirst.frc.team3618.robot.commands.TestClampComand;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	public Joystick theStickOfAwesomeness = new Joystick(1);
+	
+		public Button clamp = new JoystickButton(theStickOfAwesomeness, 1);
+		
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
@@ -34,5 +45,10 @@ public class OI {
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
+	
+	public OI(){
+		clamp.whenPressed(new TestClampComand());
+	}
+	
 }
 
